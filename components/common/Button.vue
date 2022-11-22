8338566ed2f0d59d3d064d83a0aed2ca61d1ba93
emran-jatri <emran.jatri@gmail.com>
2022-11-22 13:18:07 +0600

    button component added

---
+++ components/common/Button.vue
@@ -0,0 +1,7 @@
+<template>
+  <div>
+    <button class="bg-blue-500 text-white py-2 px-3 rounded-md mt-10">
+      <NuxtLink to="/about"> Click Me </NuxtLink>
+    </button>
+  </div>
+</template>
--- pages/index.vue
+++ pages/index.vue
@@ -2,6 +2,7 @@
   <div class="bg-gray-100 h-screen flex justify-center items-center">
     <div class="h-96 w-96 bg-white p-5 rounded-2xl shadow-xl">
       <Welcome pageName="Home" />
+			<CommonButton/>
     </div>
   </div>
 </template>
